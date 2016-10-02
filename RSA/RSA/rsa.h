600472e3eb15ab8f6916fa1958e538bc16d8a615
Dave Machado <dave@davemachado.com>
2016-10-02 01:11:13 -0400

    Update Key Class constructor

--- RSA/RSA/rsa.h
+++ RSA/RSA/rsa.h
@@ -1,24 +1,31 @@
 ﻿#ifndef RSA_H
 #define SIERPINSKI_H
 
-int getPrimeNum();
+int getPrimeNum(int numOfDigits);
+int gcd(int m, int n);
 bool checkRelativelyPrime(int num1, int num2);
+int modularInverse(int a, int b);
 
 class Key
 {
 public:
-	Key();									// default constructor
-	Key(int privateKey, int baseNum, int exp);				// Custom Key constructor
-	Key(const Key& copy_from);				// copy constructor
-	~Key();									// destructor
+	Key();
+	Key(int privateKey, int baseNum, int factor1, int factor2, int exp);
+	Key(const Key& copy_from);
+	~Key();
 
 	int getPrivateKey() const;
 	int getPublicBase() const;
+	int getFactor1() const;
+	int getFactor2() const;
 	int getPublicExp() const;
+
 
 private:
 	int _privateKey;
 	int _publicBase;
+	int _factor1;
+	int _factor2;
 	int _publicExp;
 };
 
