e9c4620f8cd8ced015c65837e8ead933d48a4a53
Dave Machado <dave@davemachado.com>
2016-10-10 22:58:25 -0400

    Add Read/Write from Key to text file

--- RSA/RSA/fileIO.cpp
+++ RSA/RSA/fileIO.cpp
@@ -33,7 +33,85 @@ void writeMessageToFile(string message, string fileName)
 		std::strcpy(arr, message.c_str());
 		file.write(arr, message.size());
 		delete[] arr;
+		file.close();
 		return;
 	}
 	else throw Exception("Output file could not be opened!");
+}
+
+Key readKey(string fileName)
+{
+	string buffer = "";
+	int privateKey, exp, base, factor1, factor2;
+	ifstream file;
+	file.open(fileName.c_str(), fstream::in);
+
+	if (file.is_open())
+	{
+		getline (file, buffer);
+		privateKey = stoi(buffer);
+		buffer.clear();
+		getline (file, buffer);
+		exp = stoi(buffer);
+		buffer.clear();
+		getline (file, buffer);
+		base = stoi(buffer);
+		buffer.clear();
+		getline (file, buffer);
+		factor1 = stoi(buffer);
+		buffer.clear();
+		getline (file, buffer);
+		factor2 = stoi(buffer);
+		buffer.clear();
+
+		file.close();
+		Key k(privateKey, base, factor1, factor2, exp);
+		return k;
+	}
+	else throw Exception("Could not open key file for reading!");
+}
+
+string writeKey(Key key)
+{
+	string str = "";
+	str.append(to_string(key.getPrivateKey()));
+	str.append("\n");
+	str.append(to_string(key.getPublicExp()));
+	str.append("\n");
+	str.append(to_string(key.getPublicBase()));
+	str.append("\n");
+	str.append(to_string(key.getFactor1()));
+	str.append("\n");
+	str.append(to_string(key.getFactor2()));
+	str.append("\n");
+	str.append("PUBLIC KEY: ");
+	string pubKey = "(" + to_string(key.getPublicBase()) + ", " + to_string(key.getPublicExp()) + ")" + "\n";
+	str.append(pubKey);
+	str.append("PRIVATE KEY: ");
+	string priKey = "(" + to_string(key.getPrivateKey()) + ")" + "\n";
+	str.append(priKey);
+
+	time_t t = time(0);   // get time now
+	struct tm * now = localtime( & t );
+
+	string keyFile = "key_";
+	keyFile.append(to_string(now->tm_year + 1900));
+	keyFile.append(to_string(now->tm_mon + 1));
+	keyFile.append(to_string(now->tm_mday));
+	keyFile.append("_");
+	keyFile.append(to_string(now->tm_hour));
+	keyFile.append(to_string(now->tm_min));
+	keyFile.append(to_string(now->tm_sec));
+	keyFile.append(".txt");
+
+	// Check if a file of this name already exists
+	ifstream ifile(keyFile);
+	if (ifile)
+	{
+		ifile.close();
+		keyFile.append("_1");
+	}
+
+	writeMessageToFile(str, keyFile);
+	return keyFile;
 }
